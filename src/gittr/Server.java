a1da8b8763b1c40f3aed779326fce88dae22ef91
Rockstar717 <warke.chetan710@gmail.com>
2023-09-16 12:06:56 +0530

    ok

---
+++ src/gittr/Server.java
@@ -0,0 +1,19 @@
+package gittr;
+
+
+public class Server {
+	
+	public void add(int a,String b) {
+		
+	}
+	
+	public int add(int a) {
+		
+		return 0;
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
